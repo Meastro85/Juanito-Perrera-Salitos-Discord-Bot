df5bb6d7d61333844cbcbb900c47f4cfd958b327
Meastro85 <verboven85@gmail.com>
2024-02-23 00:17:12 +0100

    Added interaction buttons to music commands

--- DiscordDotNet/Slash commands/SlashCommandsHandlers/AudioCommands.cs
+++ DiscordDotNet/Slash commands/SlashCommandsHandlers/AudioCommands.cs
@@ -1,13 +1,11 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
 using Discord.WebSocket;
 using DiscordDotNet.Services;
 using Lavalink4NET;
 using Lavalink4NET.Events.Players;
 using Lavalink4NET.Players;
 using Lavalink4NET.Players.Queued;
-using Lavalink4NET.Rest;
-using Lavalink4NET.Rest.Entities.Tracks;
-using Microsoft.Extensions.Options;
 
 namespace DiscordDotNet.Slash_commands.SlashCommandsHandlers;
 
@@ -16,13 +14,16 @@ public class AudioCommands : InteractionModuleBase<SocketInteractionContext>
     
     private readonly IAudioService _audioService;
     private readonly LavalinkAudioService _customService;
+    private readonly DiscordSocketClient _client;
 
-    public AudioCommands(IAudioService audioService, LavalinkAudioService service)
+    public AudioCommands(IAudioService audioService, LavalinkAudioService service, DiscordSocketClient client)
     {
         _audioService = audioService;
         _customService = service;
+        _client = client;
         _audioService.TrackEnded += TrackEndedAsync;
         _audioService.TrackStarted += TrackStartedAsync;
+        _client.ButtonExecuted += MyButtonHandler;
     }
 
     [SlashCommand("play", description: "Search and play the given song.", runMode: RunMode.Async)]
@@ -36,14 +37,12 @@ public class AudioCommands : InteractionModuleBase<SocketInteractionContext>
         {
             await FollowupAsync("You are not currently in a voice channel.").ConfigureAwait(false);
             return;
-
         }
         
-        var player = await _customService.GetPlayerAsync(Context.Guild.Id, voiceChannel.Id).ConfigureAwait(false);
+        var player = await RetrievePlayer().ConfigureAwait(false);
 
         if (player is null)
         {
-            
             return;
         }
 
@@ -61,9 +60,198 @@ public class AudioCommands : InteractionModuleBase<SocketInteractionContext>
         {
             return;
         }
-        await FollowupAsync($"Adding {track.Title} by {track.Author} to the queue.").ConfigureAwait(false);
+        await FollowupAsync($"Adding `{track.Title}` by `{track.Author}` to the queue.").ConfigureAwait(false);
     }
 
+    [SlashCommand("stop", description: "Stop the player.", runMode: RunMode.Async)]
+    public async Task Stop()
+    {
+        
+        var player = await RetrievePlayer(false).ConfigureAwait(false);
+
+        if (player is null) return;
+
+        if (player.CurrentTrack is null)
+        {
+            await RespondAsync("Nothing playing!").ConfigureAwait(false);
+            return;
+        }
+
+        await player.StopAsync().ConfigureAwait(false);
+        await RespondAsync("Stopped playing.").ConfigureAwait(false);
+
+    }
+    
+    public async Task Stop(SocketMessageComponent component)
+    {
+        
+        var player = await RetrievePlayer((ulong)component.GuildId!, false).ConfigureAwait(false);
+
+        if (player is null) return;
+
+        if (player.CurrentTrack is null)
+        {
+            await component.RespondAsync("Nothing playing!").ConfigureAwait(false);
+            return;
+        }
+
+        await player.StopAsync().ConfigureAwait(false);
+        await component.RespondAsync("Stopped playing.").ConfigureAwait(false);
+
+    }
+
+    [SlashCommand("pause", description: "Pause the music.", runMode: RunMode.Async)]
+    public async Task Pause()
+    {
+        
+
+        var player = await RetrievePlayer(false).ConfigureAwait(false);
+        if (player is null) return;
+
+        if (player.State is PlayerState.Paused)
+        {
+            await RespondAsync("The music has already been paused.").ConfigureAwait(false);
+            return;
+        }
+        
+        await player.PauseAsync().ConfigureAwait(false);
+        await RespondAsync("Paused the music.", components: GetComponents(player).Build() ).ConfigureAwait(false);
+
+    }
+    
+    public async Task Pause(SocketMessageComponent component)
+    {
+        
+
+        var player = await RetrievePlayer((ulong)component.GuildId!,false).ConfigureAwait(false);
+        if (player is null) return;
+
+        if (player.State is PlayerState.Paused)
+        {
+            await component.RespondAsync("The music has already been paused.").ConfigureAwait(false);
+            return;
+        }
+        
+        await player.PauseAsync().ConfigureAwait(false);
+        await component.RespondAsync("Paused the music.", components: GetComponents(player).Build() ).ConfigureAwait(false);
+
+    }
+
+    [SlashCommand("resume", description: "Resume the music.", runMode: RunMode.Async)]
+    public async Task Resume()
+    {
+        var player = await RetrievePlayer(false).ConfigureAwait(false);
+
+        if (player is null)
+        {
+            return;
+        }
+
+        if (player.State is not PlayerState.Paused)
+        {
+            await RespondAsync("Music is not paused.").ConfigureAwait(false);
+            return;
+        }
+
+        
+        
+        await player.ResumeAsync().ConfigureAwait(false);
+        await RespondAsync("Resuming music.", components: GetComponents(player).Build()).ConfigureAwait(false);
+    }
+
+    public async Task Resume(SocketMessageComponent component)
+    {
+        var player = await RetrievePlayer((ulong)component.GuildId!,false).ConfigureAwait(false);
+
+        if (player is null)
+        {
+            return;
+        }
+
+        if (player.State is not PlayerState.Paused)
+        {
+            await component.RespondAsync("Music is not paused.").ConfigureAwait(false);
+            return;
+        }
+        
+        await player.ResumeAsync().ConfigureAwait(false);
+        await component.RespondAsync("Resuming music.", components: GetComponents(player).Build()).ConfigureAwait(false);
+    }
+    
+    [SlashCommand("skip", description: "Skip the current song", runMode: RunMode.Async)]
+    public async Task Skip()
+    {
+        var player = await RetrievePlayer().ConfigureAwait(false);
+
+        if (player is null) return;
+
+        if (player.CurrentTrack is null)
+        {
+            await RespondAsync("Nothing is playing!").ConfigureAwait(false);
+            return;
+        }
+
+        await player.SkipAsync().ConfigureAwait(false);
+
+        var track = player.CurrentTrack;
+
+        if (track is not null)
+        {
+            await RespondAsync($"Skipped song.", components: GetComponents(player).Build()).ConfigureAwait(false);
+        }
+        else
+        {
+            await RespondAsync("Skipped. Stopped playing, queue is empty.").ConfigureAwait(false);
+        }
+
+    }
+    
+    public async Task Skip(SocketMessageComponent component)
+    {
+        var player = await RetrievePlayer((ulong)component.GuildId!,connectToVoiceChannel: false).ConfigureAwait(false);
+
+        if (player is null) return;
+
+        if (player.CurrentTrack is null)
+        {
+            await component.RespondAsync("Nothing is playing!").ConfigureAwait(false);
+            return;
+        }
+
+        await player.SkipAsync().ConfigureAwait(false);
+
+        var track = player.CurrentTrack;
+
+        if (track is not null)
+        {
+            await component.RespondAsync($"Skipped song.", components: GetComponents(player).Build()).ConfigureAwait(false);
+        }
+        else
+        {
+            await component.RespondAsync("Skipped. Stopped playing, queue is empty.").ConfigureAwait(false);
+        }
+
+    }
+
+    private async Task<QueuedLavalinkPlayer?> RetrievePlayer(bool connectToVoiceChannel = true)
+    {
+        var user = Context.Guild.GetUser(Context.User.Id);
+        var voiceChannel = user.VoiceChannel;
+        var guild = Context.Guild;
+
+        var player = await _customService.GetPlayerAsync(guild.Id, voiceChannel.Id, connectToVoiceChannel: connectToVoiceChannel);
+        
+        return player;
+    }
+    
+    private async Task<QueuedLavalinkPlayer?> RetrievePlayer(ulong guildId, bool connectToVoiceChannel = true)
+    {
+
+        var player = await _customService.GetPlayerAsync(guildId, 0, connectToVoiceChannel: connectToVoiceChannel);
+        
+        return player;
+    }
+    
     private async Task TrackEndedAsync(object obj, TrackEndedEventArgs args)
     {
         await Task.CompletedTask;
@@ -75,7 +263,53 @@ public class AudioCommands : InteractionModuleBase<SocketInteractionContext>
         {
             return;
         }
-        await FollowupAsync($"Now playing: {args.Track.Title} by {args.Track.Author}").ConfigureAwait(false);
+        
+        var builder = new ComponentBuilder().WithButton("Skip", "skipButton").WithButton("Stop","stopButton", ButtonStyle.Danger);
+        if (args.Player.State == PlayerState.Paused)
+        {
+            builder.WithButton("Resume", "resumeButton", ButtonStyle.Success);
+        }
+        else
+        {
+            builder.WithButton("Pause", "pauseButton");
+        }
+        
+        await  FollowupAsync($"Now playing: `{args.Track.Title}` by `{args.Track.Author}`", components: GetComponents(args.Player as QueuedLavalinkPlayer).Build()).ConfigureAwait(false);
     }
 
+    private ComponentBuilder GetComponents(QueuedLavalinkPlayer? player)
+    {
+        var builder = new ComponentBuilder().WithButton("Skip", "skipButton").WithButton("Stop","stopButton", ButtonStyle.Danger);
+        if (player is { State: PlayerState.Paused })
+        {
+            builder.WithButton("Resume", "resumeButton", ButtonStyle.Success);
+        }
+        else
+        {
+            builder.WithButton("Pause", "pauseButton");
+        }
+
+        return builder;
+    }
+
+    public async Task MyButtonHandler(SocketMessageComponent component)
+    {
+        
+        switch (component.Data.CustomId)
+        {
+            case "stopButton":
+                await Stop(component);
+                break;
+            case "pauseButton":
+                await Pause(component);
+                break;
+            case "resumeButton":
+                await Resume(component);
+                break;
+            case "skipButton":
+                await Skip(component);
+                break;
+        }
+    }
+    
 }
